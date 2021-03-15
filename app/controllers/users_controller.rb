430e89203346e5d1368e494050de7362fee91541
musa <mosesbwire91@gmail.com>
2021-03-15 21:27:29 +0300

    Finished form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "User succesfully created"
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
